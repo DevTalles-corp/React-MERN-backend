bbfad4ecc979d9a8e499fcd46883785645b04e0b
Fernando Herrera <fernando.herrera85@gmail.com>
2024-05-27 11:36:32 -0400

    React App ready

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const { dbConnection } = require('./database/config');
+const path = require( 'path' );
+
+const express = require( 'express' );
+require( 'dotenv' ).config();
+const cors = require( 'cors' );
+const { dbConnection } = require( './database/config' );
 
 // Crear el servidor de express
 const app = express();
@@ -10,25 +12,28 @@ const app = express();
 dbConnection();
 
 // CORS
-app.use(cors())
+app.use( cors() );
 
 // Directorio Público
-app.use( express.static('public') );
+app.use( express.static( 'public' ) );
 
 // Lectura y parseo del body
 app.use( express.json() );
 
 // Rutas
-app.use('/api/auth', require('./routes/auth') );
-app.use('/api/events', require('./routes/events') );
+app.use( '/api/auth', require( './routes/auth' ) );
+app.use( '/api/events', require( './routes/events' ) );
 
 
+app.use( '*', ( req, res ) => {
+  res.sendFile( path.join( __dirname, 'public/index.html' ) );
+} );
 
 
 // Escuchar peticiones
 app.listen( process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
-});
+  console.log( `Servidor corriendo en puerto ${ process.env.PORT }` );
+} );
 
 
 
