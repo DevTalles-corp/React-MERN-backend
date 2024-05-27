c0fcce621221ca3d726a3ea41b4d4e31e90d9ee0
Fernando Herrera <fernando.herrera85@gmail.com>
2024-05-27 08:46:40 -0400

    Backend updated

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+
+// Crear el servidor de express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+// CORS
+app.use(cors())
+
+// Directorio Público
+app.use( express.static('public') );
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas
+app.use('/api/auth', require('./routes/auth') );
+app.use('/api/events', require('./routes/events') );
+
+
+
+
+// Escuchar peticiones
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
+
+
+
+
+
+
---
+++ models/Usuario.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+
+module.exports = model('Usuario', UsuarioSchema );
+
